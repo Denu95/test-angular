4b22097ae2d17c0aa620496b6d77f735d87fab35
Giuseppe De Nuzzo <gdenuzzo@studenti.apuliadigitalmaker.it>
2023-11-28 17:36:39 +0100

    test-angular-denupeppe

---
+++ src/app/air/air.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-air',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './air.component.html',
+  styleUrl: './air.component.css'
+})
+export class AirComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AirComponent } from './air/air.component';
+import { JordanComponent } from './jordan/jordan.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomepageComponent,
+    },
+    {
+        path: 'air',
+        component: AirComponent
+    },
+    {
+        path: 'jordan',
+        component: JordanComponent
+    },
+];
